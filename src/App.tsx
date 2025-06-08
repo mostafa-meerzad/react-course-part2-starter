8bc762752d2910607d360da8475c852026a549ad
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-06-08 17:36:22 +0430

    refactor authContext to be modular and simpler to maintain

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { useReducer } from "react";
 import "./App.css";
+import AuthProvider from "./state-management/AuthProvider";
 import NavBar from "./state-management/NavBar";
 import TaskList from "./state-management/TaskList";
 import TasksContext from "./state-management/context/tasksContext";
 import taskReducer from "./state-management/reducers/taskReducer";
-import AuthContext from "./state-management/context/authContext";
-import authReducer from "./state-management/reducers/authReducer";
 
 function App() {
   const [tasks, taskDispatch] = useReducer(taskReducer, []);
-    const [user, authDispatch] = useReducer(authReducer, "");
- 
-  return (
-    <AuthContext.Provider value={{user, dispatch: authDispatch}}>
 
-    <TasksContext.Provider value={{ tasks, dispatch:taskDispatch }}>
-      <NavBar />
-      <TaskList />
-    </TasksContext.Provider>
-    </AuthContext.Provider>
+  return (
+    <AuthProvider>
+      <TasksContext.Provider value={{ tasks, dispatch: taskDispatch }}>
+        <NavBar />
+        <TaskList />
+      </TasksContext.Provider>
+    </AuthProvider>
   );
 }
 
